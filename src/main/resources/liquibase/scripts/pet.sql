61baa7513ffd74fb8b16239425ee0c486942af8d
Рамиля <bzzik@list.ru>
2023-08-26 01:40:10 +0300

    properties

---
+++ src/main/resources/liquibase/scripts/pet.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+-- changeset rziganshina: 1
+CREATE TABLE IF NOT EXISTS pet(
+);
+CREATE TABLE IF NOT EXISTS photo_pet(
+);
+CREATE TABLE IF NOT EXISTS photo_pets_report(
+);
+CREATE TABLE IF NOT EXISTS report_pet(
+);
---
+++ src/main/resources/liquibase/scripts/user.sql
@@ -0,0 +1,4 @@
+-- liquibase formatted sql
+-- changeset dsazonov: 1
+CREATE TABLE IF NOT EXISTS "user"(
+);
